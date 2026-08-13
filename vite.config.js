844805ea5de240ad767d5942b5bb8498fb9472af
jayasoftech <jayasoftdigital9@gmail.com>
2026-08-13 16:04:22 +0700

    Fix Vite build dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import oxlint from 'vite-plugin-oxlint'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/jayasoftech-website/',
+  plugins: [react(),
+            oxlint(),
+           ],
 
   server: {
     port:5174,
